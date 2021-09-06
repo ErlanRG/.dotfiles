9b77bbdf7aae7045bd5ced26a752f6bb2d24b4d5
ErlanRG <erlanrangel@gmail.com>
2021-09-06 12:40:04 -0600

    [features] Add new nvim settings
    - Add 'set hidden' as a default setting
    - Fix keymappings

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -4,12 +4,15 @@ local opt = vim.o
 cmd('syntax on')
 cmd('set nowrap')
 cmd('set ts=4')
+cmd('set sts=4')
 cmd('set sw=4')
 cmd('set expandtab')
 cmd('set number')
 cmd('set relativenumber')
 cmd('set smartindent')
 cmd('set nohlsearch')
+cmd('set hidden')
+cmd('set shortmess+=c')
 cmd('filetype plugin on')
 cmd('colorscheme onedark')
 
@@ -20,13 +23,14 @@ opt.pumheight = 10
 opt.mouse = "a"
 opt.cursorline = true
 opt.showtabline = 2
+opt.scrolloff = 8
 opt.termguicolors = true
 opt.showmode = false
 opt.backup = false
 opt.writebackup = false
 opt.swapfile = false
--- opt.undodir = ~/.undo/undo
 opt.undofile = true
+-- opt.undodir = "~/.undo/undodir"
 opt.signcolumn = "yes"
 opt.updatetime = 300
 opt.clipboard = "unnamedplus"
