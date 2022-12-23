d94ee49380c559c19e84a281d6f843a2a239758d
Erlan Rangel <erlanrangel@gmail.com>
2022-12-23 13:48:10 -0600

    update options

--- nvim/.config/nvim/lua/rangeler/keymaps.lua
+++ nvim/.config/nvim/lua/rangeler/keymaps.lua
@@ -16,11 +16,11 @@ keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- Creating splits
-keymap("n", "<Leader>vs", "<cmd>vnew<CR>", opts)
-keymap("n", "<Leader>hs", "<cmd>new<CR>", opts)
+keymap("n", "<Leader>vs", vim.cmd.vnew, opts)
+keymap("n", "<Leader>hs", vim.cmd.new, opts)
 
 -- Save
-keymap("n", "<Leader>w", "<cmd>w<CR>", opts)
+keymap("n", "<Leader>w", vim.cmd.w, opts)
 
 -- Resize with arrows
 keymap("n", "<C-Up>", "<cmd>resize +2<CR>", opts)
@@ -29,11 +29,11 @@ keymap("n", "<C-Left>", "<cmd>vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", "<cmd>vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", "<cmd>bnext<CR>", opts)
-keymap("n", "<S-h>", "<cmd>bprevious<CR>", opts)
+keymap("n", "<S-l>", vim.cmd.bnext, opts)
+keymap("n", "<S-h>", vim.cmd.bprevious, opts)
 
 -- Close buffers
-keymap("n", "<Leader>c", "<cmd>bd<CR>", opts)
+keymap("n", "<Leader>c", require("rangeler.options").buf_kill, opts)
 
 -- Insert --
 -- Press jk fast to enter
@@ -48,8 +48,8 @@ keymap("v", ">", ">gv", opts)
 -- Move text up and down
 keymap("n", "<A-j>", ":m .+1<CR>==", opts)
 keymap("n", "<A-k>", ":m .-2<CR>==", opts)
-keymap("v", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("v", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("v", "J", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "K", ":m '<-2<CR>gv=gv", opts)
 
 -- Others
 -- Yank the rest of the line with "Y"
@@ -64,10 +64,27 @@ keymap("n", "<Leader>fg", "<cmd>Telescope git_commits<CR>", opts)
 keymap("n", "<Leader>fb", "<cmd>Telescope buffers<CR>", opts)
 
 -- Nvimtree
-keymap("n", "<Leader>e", "<cmd>NvimTreeToggle<CR>", opts)
+keymap("n", "<Leader>e", vim.cmd.NvimTreeToggle, opts)
 
 -- Tablemode
-keymap("x", "<Leader>tb", "<cmd>Tableize<CR>", opts)
+keymap("x", "<Leader>tb", vim.cmd.Tableize, opts)
 
 -- Packer
-keymap("n", "<Leader>ps", "<cmd>PackerSync<CR>", opts)
+keymap("n", "<Leader>ps", vim.cmd.PackerSync, opts)
+
+-- Autoformat
+keymap("n", "<Leader>lf", vim.lsp.buf.format, opts)
+
+-- C-u / C-d keep centered
+keymap("n", "<C-d>", "<C-d>zz", opts)
+keymap("n", "<C-u>", "<C-u>zz", opts)
+
+-- J keeps cursor in place
+keymap("n", "J", "mzJ`z", opts)
+
+-- Search now keep cursor in the middle
+keymap("n", "n", "nzzzv")
+keymap("n", "N", "Nzzzv")
+
+-- Not sure how to comment this one
+keymap("x", "<leader>p", "\"_dP", opts)
