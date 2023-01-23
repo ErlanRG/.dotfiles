5a18527a4b39f0a2c6f4e9fa018adda46b25f2f7
Erlan Rangel <erlanrangel@gmail.com>
2023-01-23 15:56:06 -0600

    add wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,201 @@
+-- https://github.com/thanhvule0310/dotfiles/blob/main/config/wezterm/wezterm.lua
+
+local wezterm = require "wezterm"
+
+local catppuccin_colors = {
+  rosewater = "#F5E0DC",
+  flamingo = "#F2CDCD",
+  pink = "#F5C2E7",
+  mauve = "#CBA6F7",
+  red = "#F38BA8",
+  maroon = "#EBA0AC",
+  peach = "#FAB387",
+  yellow = "#F9E2AF",
+  green = "#A6E3A1",
+  teal = "#94E2D5",
+  sky = "#89DCEB",
+  sapphire = "#74C7EC",
+  blue = "#89B4FA",
+  lavender = "#B4BEFE",
+
+  text = "#CDD6F4",
+  subtext1 = "#BAC2DE",
+  subtext0 = "#A6ADC8",
+  overlay2 = "#9399B2",
+  overlay1 = "#7F849C",
+  overlay0 = "#6C7086",
+  surface2 = "#585B70",
+  surface1 = "#45475A",
+  surface0 = "#313244",
+
+  base = "#1E1E2E",
+  mantle = "#181825",
+  crust = "#11111B",
+}
+
+local function get_process(tab)
+  local process_icons = {
+    ["docker"] = {
+      { Foreground = { Color = catppuccin_colors.blue } },
+      { Text = wezterm.nerdfonts.linux_docker },
+    },
+    ["docker-compose"] = {
+      { Foreground = { Color = catppuccin_colors.blue } },
+      { Text = wezterm.nerdfonts.linux_docker },
+    },
+    ["nvim"] = {
+      { Foreground = { Color = catppuccin_colors.green } },
+      { Text = wezterm.nerdfonts.custom_vim },
+    },
+    ["vim"] = {
+      { Foreground = { Color = catppuccin_colors.green } },
+      { Text = wezterm.nerdfonts.dev_vim },
+    },
+    ["node"] = {
+      { Foreground = { Color = catppuccin_colors.green } },
+      { Text = wezterm.nerdfonts.mdi_hexagon },
+    },
+    ["zsh"] = {
+      { Foreground = { Color = catppuccin_colors.peach } },
+      { Text = wezterm.nerdfonts.dev_terminal },
+    },
+    ["bash"] = {
+      { Foreground = { Color = catppuccin_colors.subtext0 } },
+      { Text = wezterm.nerdfonts.cod_terminal_bash },
+    },
+    ["yay"] = {
+      { Foreground = { Color = catppuccin_colors.lavender } },
+      { Text = wezterm.nerdfonts.linux_archlinux },
+    },
+    ["htop"] = {
+      { Foreground = { Color = catppuccin_colors.yellow } },
+      { Text = wezterm.nerdfonts.mdi_chart_donut_variant },
+    },
+    ["cargo"] = {
+      { Foreground = { Color = catppuccin_colors.peach } },
+      { Text = wezterm.nerdfonts.dev_rust },
+    },
+    ["lazydocker"] = {
+      { Foreground = { Color = catppuccin_colors.blue } },
+      { Text = wezterm.nerdfonts.linux_docker },
+    },
+    ["git"] = {
+      { Foreground = { Color = catppuccin_colors.peach } },
+      { Text = wezterm.nerdfonts.dev_git },
+    },
+    ["lazygit"] = {
+      { Foreground = { Color = catppuccin_colors.peach } },
+      { Text = wezterm.nerdfonts.dev_git },
+    },
+    ["lua"] = {
+      { Foreground = { Color = catppuccin_colors.blue } },
+      { Text = wezterm.nerdfonts.seti_lua },
+    },
+    ["wget"] = {
+      { Foreground = { Color = catppuccin_colors.yellow } },
+      { Text = wezterm.nerdfonts.mdi_arrow_down_box },
+    },
+    ["curl"] = {
+      { Foreground = { Color = catppuccin_colors.yellow } },
+      { Text = wezterm.nerdfonts.mdi_flattr },
+    },
+    ["gh"] = {
+      { Foreground = { Color = catppuccin_colors.mauve } },
+      { Text = wezterm.nerdfonts.dev_github_badge },
+    },
+  }
+
+  local process_name = string.gsub(tab.active_pane.foreground_process_name, "(.*[/\\])(.*)", "%2")
+
+  return wezterm.format(
+    process_icons[process_name]
+      or { { Foreground = { Color = catppuccin_colors.sky } }, { Text = string.format("[%s]", process_name) } }
+  )
+end
+
+local function get_current_working_dir(tab)
+  local current_dir = tab.active_pane.current_working_dir
+  local HOME_DIR = string.format("file://%s", os.getenv "HOME")
+
+  return current_dir == HOME_DIR and "  ~"
+    or string.format("  %s", string.gsub(current_dir, "(.*[/\\])(.*)", "%2"))
+end
+
+wezterm.on("format-tab-title", function(tab)
+  return wezterm.format {
+    { Attribute = { Intensity = "Half" } },
+    { Text = string.format(" %s  ", tab.tab_index + 1) },
+    "ResetAttributes",
+    { Text = get_process(tab) },
+    { Text = " " },
+    { Text = get_current_working_dir(tab) },
+    { Foreground = { Color = catppuccin_colors.base } },
+    { Text = "  ▕" },
+  }
+end)
+
+return {
+  audible_bell = "Disabled",
+  color_scheme = "Catppuccin Mocha",
+  disable_default_key_bindings = false,
+  enable_scroll_bar = false,
+  font_size = 10,
+  hide_tab_bar_if_only_one_tab = true,
+  show_new_tab_button_in_tab_bar = false,
+  tab_bar_at_bottom = true,
+  tab_max_width = 50,
+  use_fancy_tab_bar = false,
+  window_background_opacity = 1.0,
+  window_decorations = "RESIZE",
+  inactive_pane_hsb = {
+    brightness = 0.6,
+    saturation = 1.0,
+  },
+  colors = {
+    background = catppuccin_colors.base,
+    split = catppuccin_colors.surface0,
+    tab_bar = {
+      background = catppuccin_colors.crust,
+    },
+  },
+  window_frame = {
+    font = wezterm.font { family = "JetBrains" },
+    font_size = 10,
+  },
+  window_padding = {
+    left = 2,
+    right = 2,
+  },
+  keys = {
+    -- Open/close tabs
+    { mods = "ALT", key = "w", action = wezterm.action { SpawnTab = "CurrentPaneDomain" } },
+    { mods = "ALT", key = "Q", action = wezterm.action { CloseCurrentTab = { confirm = true } } },
+    { mods = "ALT", key = "q", action = wezterm.action { CloseCurrentPane = { confirm = true } } },
+    -- Window movement
+    { mods = "ALT", key = "h", action = wezterm.action { ActivateTabRelative = -1 } },
+    { mods = "ALT", key = "l", action = wezterm.action { ActivateTabRelative = 1 } },
+    -- Splits
+    { mods = "CTRL|ALT", key = "h", action = wezterm.action { SplitHorizontal = { domain = "CurrentPaneDomain" } } },
+    { mods = "CTRL|ALT", key = "v", action = wezterm.action { SplitVertical = { domain = "CurrentPaneDomain" } } },
+
+    { mods = "ALT|SHIFT", key = "h", action = wezterm.action.ActivatePaneDirection "Left" },
+    { mods = "ALT|SHIFT", key = "j", action = wezterm.action.ActivatePaneDirection "Down" },
+    { mods = "ALT|SHIFT", key = "k", action = wezterm.action.ActivatePaneDirection "Up" },
+    { mods = "ALT|SHIFT", key = "l", action = wezterm.action.ActivatePaneDirection "Right" },
+    -- Resize
+    { mods = "CTRL|ALT", key = "LeftArrow", action = wezterm.action.AdjustPaneSize { "Left", 1 } },
+    { mods = "CTRL|ALT", key = "DownArrow", action = wezterm.action.AdjustPaneSize { "Down", 1 } },
+    { mods = "CTRL|ALT", key = "RightArrow", action = wezterm.action.AdjustPaneSize { "Right", 1 } },
+    { mods = "CTRL|ALT", key = "UpArrow", action = wezterm.action.AdjustPaneSize { "Up", 1 } },
+    -- Move to tab
+    { mods = "ALT", key = "1", action = wezterm.action { ActivateTab = 0 } },
+    { mods = "ALT", key = "2", action = wezterm.action { ActivateTab = 1 } },
+    { mods = "ALT", key = "3", action = wezterm.action { ActivateTab = 2 } },
+    { mods = "ALT", key = "4", action = wezterm.action { ActivateTab = 3 } },
+    { mods = "ALT", key = "5", action = wezterm.action { ActivateTab = 4 } },
+    { mods = "ALT", key = "6", action = wezterm.action { ActivateTab = 5 } },
+    { mods = "ALT", key = "7", action = wezterm.action { ActivateTab = 6 } },
+    { mods = "ALT", key = "8", action = wezterm.action { ActivateTab = 7 } },
+    { mods = "ALT", key = "9", action = wezterm.action { ActivateTab = 8 } },
+  },
+}
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -18,10 +18,10 @@ DISABLE_UPDATE_PROMPT="true"
 
 # Plugins
 plugins=(
-    git
-    zsh-autosuggestions
-    zsh-syntax-highlighting
-    vi-mode
+  git
+  zsh-autosuggestions
+  zsh-syntax-highlighting
+  vi-mode
 )
 
 # Oh-my-zsh
@@ -45,7 +45,7 @@ alias la='lsd -A'
 alias lm='lsd -m'
 alias lr='lsd -R'
 alias ls='exa -lhF --color=always --icons --sort=size --group-directories-first'
-alias l='exa -lahF --color=always --icons --sort=size --group-directories-first'
+alias l='exa -lahF --color=always --icons --sort=size --group-directories-first -s=Name'
 
 # git
 alias gcl='git clone --depth 1'
@@ -83,22 +83,22 @@ alias tmuxconf='nvim $HOME/.config/tmux/tmux.conf'
 
 # navigation
 up () {
-    local d=""
-    local limit="$1"
+  local d=""
+  local limit="$1"
 
-    # Default to limit of 1
-    if [ -z "$limit" ] || [ "$limit" -le 0 ]; then
-        limit=1
-    fi
+  # Default to limit of 1
+  if [ -z "$limit" ] || [ "$limit" -le 0 ]; then
+    limit=1
+  fi
 
-    for ((i=1;i<=limit;i++)); do
-        d="../$d"
-    done
+  for ((i=1;i<=limit;i++)); do
+    d="../$d"
+  done
 
-    # perform cd. Show error if cd fails
-    if ! cd "$d"; then
-        echo "Couldn't go up $limit dirs.";
-    fi
+  # perform cd. Show error if cd fails
+  if ! cd "$d"; then
+    echo "Couldn't go up $limit dirs.";
+  fi
 }
 
 # Fzf
@@ -106,93 +106,93 @@ up () {
 # ex: cf word1 word2 ... (even part of a file name)
 # zsh autoload function
 cf() {
-    local file
+  local file
 
-    file="$(locate -Ai -0 $@ | grep -z -vE '~$' | fzf-tmux -p 80%,60% --read0 -0 -1)"
+  file="$(locate -Ai -0 $@ | grep -z -vE '~$' | fzf-tmux -p 80%,60% --read0 -0 -1)"
 
-    if [[ -n $file ]]
+  if [[ -n $file ]]
+  then
+    if [[ -d $file ]]
     then
-        if [[ -d $file ]]
-        then
-            cd -- $file
-        else
-            cd -- ${file:h}
-        fi
+      cd -- $file
+    else
+      cd -- ${file:h}
     fi
+  fi
 }
 
 # Just use extract
 function extract ()
 {
-    __pkgtools__at_function_enter extract
-    local remove_archive
-    local success
-    local file_name
-    local extract_dir
+  __pkgtools__at_function_enter extract
+  local remove_archive
+  local success
+  local file_name
+  local extract_dir
 
-    if [[ "$1" == "" ]]; then
-        echo "Usage: extract [-option] [file ...]"
-        echo
-        echo "Options:"
-        echo "    -r, --remove : Remove archive."
-        echo
+  if [[ "$1" == "" ]]; then
+    echo "Usage: extract [-option] [file ...]"
+    echo
+    echo "Options:"
+    echo "    -r, --remove : Remove archive."
+    echo
+  fi
+
+  remove_archive=1
+  if [[ "$1" == "-r" ]] || [[ "$1" == "--remove" ]]; then
+    remove_archive=0
+    shift
+  fi
+
+  while [ -n "$1" ]; do
+    if [[ ! -f "$1" ]]; then
+      pkgtools__msg_warning "'$1' is not a valid file"
+      shift
+      continue
     fi
 
-    remove_archive=1
-    if [[ "$1" == "-r" ]] || [[ "$1" == "--remove" ]]; then
-        remove_archive=0
-        shift
-    fi
+    success=0
+    file_name="$( basename "$1" )"
+    extract_dir="$( echo "$file_name" | sed "s/\.${1##*.}//g" )"
+    case "$1" in
+        (*.tar.gz|*.tgz) tar xvzf "$1" ;;
+        (*.tar.bz2|*.tbz|*.tbz2) tar xvjf "$1" ;;
+        (*.tar.xz|*.txz) tar --xz --help &> /dev/null \
+          && tar --xz -xvf "$1" \
+          || xzcat "$1" | tar xvf - ;;
+        (*.tar.zma|*.tlz) tar --lzma --help &> /dev/null \
+          && tar --lzma -xvf "$1" \
+          || lzcat "$1" | tar xvf - ;;
+        (*.tar) tar xvf "$1" ;;
+        (*.gz) gunzip "$1" ;;
+        (*.bz2) bunzip2 "$1" ;;
+        (*.xz) unxz "$1" ;;
+        (*.lzma) unlzma "$1" ;;
+        (*.Z) uncompress "$1" ;;
+        (*.zip) unzip "$1" -d $extract_dir ;;
+        (*.rar) unrar e -ad "$1" ;;
+        (*.7z) 7za x "$1" ;;
+        (*.deb)
+        mkdir -p "$extract_dir/control"
+        mkdir -p "$extract_dir/data"
+        cd "$extract_dir"; ar vx "../${1}" > /dev/null
+        cd control; tar xzvf ../control.tar.gz
+        cd ../data; tar xzvf ../data.tar.gz
+        cd ..; rm *.tar.gz debian-binary
+        cd ..
+        ;;
+        (*)
+        pkgtools__msg_error "'$1' cannot be extracted" 1>&2
+        success=1
+        ;;
+    esac
 
-    while [ -n "$1" ]; do
-        if [[ ! -f "$1" ]]; then
-            pkgtools__msg_warning "'$1' is not a valid file"
-            shift
-            continue
-        fi
-
-        success=0
-        file_name="$( basename "$1" )"
-        extract_dir="$( echo "$file_name" | sed "s/\.${1##*.}//g" )"
-        case "$1" in
-                (*.tar.gz|*.tgz) tar xvzf "$1" ;;
-                (*.tar.bz2|*.tbz|*.tbz2) tar xvjf "$1" ;;
-                (*.tar.xz|*.txz) tar --xz --help &> /dev/null \
-                    && tar --xz -xvf "$1" \
-                    || xzcat "$1" | tar xvf - ;;
-                (*.tar.zma|*.tlz) tar --lzma --help &> /dev/null \
-                    && tar --lzma -xvf "$1" \
-                    || lzcat "$1" | tar xvf - ;;
-                (*.tar) tar xvf "$1" ;;
-                (*.gz) gunzip "$1" ;;
-                (*.bz2) bunzip2 "$1" ;;
-                (*.xz) unxz "$1" ;;
-                (*.lzma) unlzma "$1" ;;
-                (*.Z) uncompress "$1" ;;
-                (*.zip) unzip "$1" -d $extract_dir ;;
-                (*.rar) unrar e -ad "$1" ;;
-                (*.7z) 7za x "$1" ;;
-                (*.deb)
-                mkdir -p "$extract_dir/control"
-                mkdir -p "$extract_dir/data"
-                cd "$extract_dir"; ar vx "../${1}" > /dev/null
-                cd control; tar xzvf ../control.tar.gz
-                cd ../data; tar xzvf ../data.tar.gz
-                cd ..; rm *.tar.gz debian-binary
-                cd ..
-                ;;
-                (*)
-                pkgtools__msg_error "'$1' cannot be extracted" 1>&2
-                success=1
-                ;;
-        esac
-
-        (( success = $success > 0 ? $success : $? ))
-        (( $success == 0 )) && (( $remove_archive == 0 )) && rm "$1"
-        shift
-    done
-    __pkgtools__at_function_exit
-    return 0
+    (( success = $success > 0 ? $success : $? ))
+    (( $success == 0 )) && (( $remove_archive == 0 )) && rm "$1"
+    shift
+  done
+  __pkgtools__at_function_exit
+  return 0
 }
 
 eval "$(starship init zsh)"
