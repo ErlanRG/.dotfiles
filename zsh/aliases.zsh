f78ba049d590ff4d7e2586f15b635a2f8170bcc6
Erlan Rangel <erlanrangel@gmail.com>
2023-05-22 23:03:36 -0600

    add(zsh): Add new and fix some aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,31 +1,37 @@
 # ALIASES
 # omz
-alias zshc="nvim ~/.zshrc"
-alias sz='source $HOME/.zshrc'
 alias ohmyzsh="nvim ~/.oh-my-zsh"
+alias sz='source $HOME/.zshrc'
+alias zshc="nvim ~/.zshrc"
 
 # ls
+alias l='exa -lahF --color=always --icons --sort=size --group-directories-first -s=Name'
 alias ll='exa -lah'
 alias ls='exa -lhF --color=always --icons --sort=size --group-directories-first'
-alias l='exa -lahF --color=always --icons --sort=size --group-directories-first -s=Name'
 
 # git
 alias ga='git add'
 alias gap='git add -p'
 alias gc='git commit'
+alias gcb='git checkout -b'
+alias gcl='git clone --depth=1'
 alias gco='git checkout $(git branch | fzf-tmux -p 20%,50%)'
 alias gd='git diff'
 alias gdb='git branch -D'
 alias gfp='git fetch -p origin'
-alias gl='git log --graph'
+alias gl='git log --oneline --decorate --graph'
 alias gp='git push'
+alias gs='git status'
+alias gundo='git reset --soft HEAD~1'
 alias lg='lazygit'
 
 # pacman and yay
+alias cleanup='sudo pacman -Rns $(pacman -Qtdq)'  # remove orphaned packages
 alias pacsyu='sudo pacman -Syu'                  # update only standard pkgs
 alias pacsyyu='sudo pacman -Syyu'                # Refresh pkglist & update standard pkgs
 alias unlock='sudo rm /var/lib/pacman/db.lck'    # remove pacman lock
-alias cleanup='sudo pacman -Rns (pacman -Qtdq)'  # remove orphaned packages
+alias yaysua='yay -Sua --noconfirm'              # update only AUR pkgs (yay)
+alias yaysyu='yay -Syu --noconfirm'              # update standard pkgs and AUR pkgs (yay)
 
 # confirm before overwriting something
 alias cp="cp -i"
@@ -34,11 +40,11 @@ alias rm='rm -i'
 
 # other
 alias c='clear'
-alias startup_nvim='nvim --startuptime startup.log -c exit && tail -100 startup.log'
 alias shutdown='shutdown now'
+alias startup_nvim='nvim --startuptime startup.log -c exit && tail -100 startup.log'
 
 # Tmux
-alias tls='tmux list-sessions'
+alias tconf='nvim $HOME/.config/tmux/tmux.conf'
 alias tk='tmux kill-server'
 alias tks='tmux kill-session -a' # kill all sessions but the current
-alias tconf='nvim $HOME/.config/tmux/tmux.conf'
+alias tls='tmux list-sessions'
