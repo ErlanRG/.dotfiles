3714b4294a36c33c661d33ec885ab5b45a1aec17
Erlan Rangel <erlanrangel@gmail.com>
2024-03-17 11:01:06 -0600

    update(picom): using `picom-ftlabs` fork to enable animations
    - Update the build script to include these changes

--- build.sh
+++ build.sh
@@ -76,13 +76,13 @@ install_fzf() {
 
 install_dependencies() {
     banner "Installing dependencies from Arch Repos"
-    sudo pacman -S --needed dunst eza git kitty lazygit nitrogen picom polybar rofi starship stow thunar tmux yay zoxide zsh --noconfirm || {
+    sudo pacman -S --needed dunst eza git kitty lazygit nitrogen polybar rofi starship stow thunar tmux yay zoxide zsh --noconfirm || {
         echo "Error: Failed to install packages."
         exit 1
     }
 
     banner "Installing dependencies from the AUR"
-    yay -S --needed betterlockscreen librewolf-bin autotiling --noconfirm || {
+    yay -S --needed librewolf-bin autotiling picom-ftlabs-git betterlockscreen --noconfirm || {
         echo "Error: Failed to install packages"
         exit 1
     }
