ed7a6de129f70f06de77a6cbeb4da28e2bae8892
Erlan Rangel <erlanrangel@gmail.com>
2024-05-22 08:50:04 -0600

    update: move autocmds for specific filetypes to after/ftplugin

--- nvim/.config/nvim/lua/core/autocmds.lua
+++ nvim/.config/nvim/lua/core/autocmds.lua
@@ -51,14 +51,6 @@ autocmd("FileType", {
   command = "setlocal spell",
 })
 
---[[C++]]
-local CPP = augroup("CPPGroup", { clear = true })
-autocmd("FileType", {
-  pattern = "cpp",
-  group = CPP,
-  command = "setlocal tabstop=4 shiftwidth=4 expandtab",
-})
-
 --[[ Auto resize ]]
 local AutoResize = augroup("AutoResizeGroup", { clear = true })
 autocmd("VimResized", {
