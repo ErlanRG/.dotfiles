0386ef82ee5791cc780aa45015f694ea683b3b38
Erlan Rangel <erlanrangel@gmail.com>
2025-02-03 10:49:17 -0600

    fix(gitsigns): moved on_attach keys to keys table

--- nvim/.config/nvim/lua/rangeler/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/rangeler/plugins/gitsigns.lua
@@ -44,55 +44,127 @@ return {
                 col = 1,
             },
         },
-        on_attach = function(bufnr)
-            local gitsigns = require 'gitsigns'
-
-            local function map(mode, l, r, opts)
-                opts = opts or {}
-                opts.buffer = bufnr
-                vim.keymap.set(mode, l, r, opts)
-            end
-
+        keys = {
             -- Navigation
-            map('n', ']c', function()
-                if vim.wo.diff then
-                    vim.cmd.normal { ']c', bang = true }
-                else
-                    gitsigns.nav_hunk 'next'
-                end
-            end, { desc = 'Jump to next git [c]hange' })
-
-            map('n', '[c', function()
-                if vim.wo.diff then
-                    vim.cmd.normal { '[c', bang = true }
-                else
-                    gitsigns.nav_hunk 'prev'
-                end
-            end, { desc = 'Jump to previous git [c]hange' })
-
+            {
+                ']c',
+                function()
+                    if vim.wo.diff then
+                        vim.cmd.normal { ']c', bang = true }
+                    else
+                        require('gitsigns').nav_hunk 'next'
+                    end
+                end,
+                desc = 'Jump to next git [c]hange',
+            },
+            {
+                '[c',
+                function()
+                    if vim.wo.diff then
+                        vim.cmd.normal { '[c', bang = true }
+                    else
+                        require('gitsigns').nav_hunk 'prev'
+                    end
+                end,
+                desc = 'Jump to previous git [c]hange',
+            },
             -- Actions
             -- visual mode
-            map('v', '<leader>hs', function()
-                gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-            end, { desc = 'git [s]tage hunk' })
-            map('v', '<leader>hr', function()
-                gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
-            end, { desc = 'git [r]eset hunk' })
+            {
+                '<leader>hs',
+                mode = 'v',
+                function()
+                    require('gitsigns').stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+                end,
+                desc = 'git [s]tage hunk',
+            },
+            {
+                '<leader>hr',
+                mode = 'v',
+                function()
+                    require('gitsigns').reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+                end,
+                desc = 'git [r]eset hunk',
+            },
             -- normal mode
-            map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'git [s]tage hunk' })
-            map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'git [r]eset hunk' })
-            map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'git [S]tage buffer' })
-            map('n', '<leader>hu', gitsigns.stage_hunk, { desc = 'git [u]ndo stage hunk' })
-            map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
-            map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
-            map('n', '<leader>hb', gitsigns.blame_line, { desc = 'git [b]lame line' })
-            map('n', '<leader>hd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
-            map('n', '<leader>hD', function()
-                gitsigns.diffthis '@'
-            end, { desc = 'git [D]iff against last commit' })
+            {
+                '<leader>hs',
+                function()
+                    require('gitsigns').stage_hunk()
+                end,
+                desc = 'git [s]tage hunk',
+            },
+            {
+                '<leader>hr',
+                function()
+                    require('gitsigns').reset_hunk()
+                end,
+                desc = 'git [r]eset hunk',
+            },
+            {
+                '<leader>hS',
+                function()
+                    require('gitsigns').stage_buffer()
+                end,
+                desc = 'git [S]tage buffer',
+            },
+            {
+                '<leader>hu',
+                function()
+                    require('gitsigns').stage_hunk()
+                end,
+                desc = 'git [u]ndo stage hunk',
+            },
+            {
+                '<leader>hR',
+                function()
+                    require('gitsigns').reset_buffer()
+                end,
+                desc = 'git [R]eset buffer',
+            },
+            {
+                '<leader>hp',
+                function()
+                    require('gitsigns').preview_hunk()
+                end,
+                desc = 'git [p]review hunk',
+            },
+            {
+                '<leader>hb',
+                function()
+                    require('gitsigns').blame_line()
+                end,
+                desc = 'git [b]lame line',
+            },
+            {
+                '<leader>hd',
+                function()
+                    require('gitsigns').diffthis()
+                end,
+                desc = 'git [d]iff against index',
+            },
+            {
+                '<leader>hD',
+                function()
+                    require('gitsigns').diffthis() '@'
+                end,
+                desc = 'git [D]iff against last commit',
+            },
             -- Toggles
-            map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
-            map('n', '<leader>tD', gitsigns.preview_hunk_inline, { desc = '[T]oggle git show [D]eleted' })
-        end,
+            {
+                '<leader>tb',
+                function()
+                    require('gitsigns').toggle_current_line_blame()
+                end,
+                desc = '[T]oggle git show [b]lame line',
+            },
+            {
+                '<leader>tD',
+                function()
+                    require('gitsigns').preview_hunk_inline()
+                end,
+                desc = '[T]oggle git show [D]eleted',
+            },
+        },
     },
 }
