d54515bd80dc1b5a09056d7a0e2eddd3dd26e412
Erlan Rangel <erlanrangel@gmail.com>
2025-07-15 11:27:42 -0600

    fix(nvim): remove Comment hl restrictions

--- nvim/.config/nvim/lua/rangeler/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/rangeler/plugins/colorscheme.lua
@@ -4,9 +4,7 @@ return {
     'catppuccin/nvim',
     name = 'catppuccin',
     priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
+    config = function()
         vim.cmd.colorscheme 'catppuccin'
-
-        vim.cmd.hi 'Comment gui=none'
     end,
 }
