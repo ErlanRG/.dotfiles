130d44b2734ca4afc53032bfde30932cfa84da4f
Erlan Rangel <erlanrangel@gmail.com>
2025-07-14 21:23:34 -0600

    refactor(nvim): conform lsp format function depending on filetype

--- nvim/.config/nvim/lua/rangeler/plugins/conform.lua
+++ nvim/.config/nvim/lua/rangeler/plugins/conform.lua
@@ -21,16 +21,16 @@ return {
             local disable_filetypes = {}
             local lsp_format_opt
             if disable_filetypes[vim.bo[bufnr].filetype] then
-                lsp_format_opt = 'never'
+                return nil
             else
-                lsp_format_opt = 'fallback'
+                return {
+                    timeout_ms = 500,
+                    lsp_format = lsp_format_opt,
+                }
             end
-            return {
-                timeout_ms = 500,
-                lsp_format = lsp_format_opt,
-            }
         end,
         formatters_by_ft = {
+            c = { 'clang-format' },
             lua = { 'stylua' },
             -- Conform can also run multiple formatters sequentially
             -- python = { "isort", "black" },
