e4718c62bb9779933dc75b6efe71c5c3a9974c66
Erlan Rangel <erlanrangel@gmail.com>
2025-07-15 19:57:44 -0600

    feat(nvim): add lsp progress notification

--- nvim/.config/nvim/lua/rangeler/config/autocmds.lua
+++ nvim/.config/nvim/lua/rangeler/config/autocmds.lua
@@ -131,3 +131,19 @@ vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
         vim.fn.setpos('.', save_cursor)
     end,
 })
+
+-- LSP Progress notification
+vim.api.nvim_create_autocmd('LspProgress', {
+    ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+    callback = function(ev)
+        local spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' }
+        vim.notify(vim.lsp.status(), 'info', {
+            id = 'lsp_progress',
+            title = 'LSP Progress',
+            opts = function(notif)
+                local icons = require 'rangeler.utils.icons'
+                notif.icon = ev.data.params.value.kind == 'end' and icons.ui.Check .. ' ' or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+            end,
+        })
+    end,
+})
