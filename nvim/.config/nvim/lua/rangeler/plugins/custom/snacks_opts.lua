16d946b5d43423aae1e4639306e3d195da2fc92c
Erlan Rangel <erlanrangel@gmail.com>
2025-07-21 09:49:49 -0600

    update(nvim): add Mason option to dashboard

--- nvim/.config/nvim/lua/rangeler/plugins/custom/snacks_opts.lua
+++ nvim/.config/nvim/lua/rangeler/plugins/custom/snacks_opts.lua
@@ -3,6 +3,18 @@ local icons = require 'rangeler.utils.icons'
 
 -- Dashboard
 M.dashboard = {
+    preset = {
+        keys = {
+            { icon = ' ', key = 'f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = ' ', key = 'n', desc = 'New File', action = ':ene | startinsert' },
+            { icon = ' ', key = 'g', desc = 'Find Text', action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = ' ', key = 'r', desc = 'Recent Files', action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            { icon = ' ', key = 'c', desc = 'Config', action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+            { icon = '󰒲 ', key = 'L', desc = 'Lazy', action = ':Lazy', enabled = package.loaded.lazy ~= nil },
+            { icon = ' ', key = 'M', desc = 'Mason', action = ':Mason' },
+            { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
+        },
+    },
     sections = {
         { section = 'header' },
         { section = 'keys', gap = 1, padding = 1 },
