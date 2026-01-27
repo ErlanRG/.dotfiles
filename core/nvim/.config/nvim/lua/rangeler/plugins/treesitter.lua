5fab53e703379238fb29a41165eb02151b3b90f0
Erlan Rangel <erlanrangel@gmail.com>
2026-01-26 20:00:57 -0600

    fix(nvim): treesitter plugin module is now called 'config'

--- core/nvim/.config/nvim/lua/rangeler/plugins/treesitter.lua
+++ core/nvim/.config/nvim/lua/rangeler/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     event = 'VeryLazy',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    main = 'nvim-treesitter.config', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
         ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
