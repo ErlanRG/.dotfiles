c0fc2f0e8bcd3394a055610bc744185c082d79b1
Erlan Rangel <erlanrangel@gmail.com>
2026-01-30 11:08:33 -0600

    feat!: change rofi in favor of vicinae
    This commit changes the menu launcher from rofi to vicinae. These
    changes include:
    - Application launcher applet
    - Switch windows menu
    - Monitor selection applet

--- wms/i3/screenlayout/.screenlayout/monitor_applet.sh
+++ wms/i3/screenlayout/.screenlayout/monitor_applet.sh
@@ -1,74 +1,33 @@
 #!/usr/bin/env bash
 
-## Author  : Aditya Shakya (adi1090x)
-## Github  : @adi1090x
-#
-## Applets : Quick Links
-
-# Import Current Theme
-source "$HOME"/.config/rofi/applets/shared/theme.bash
-theme="$type/$style"
-
-# Theme Elements
-prompt='Display config'
-mesg="Choose display configuration:"
-
-if [[ ( "$theme" == *'type-1'* ) || ( "$theme" == *'type-3'* ) || ( "$theme" == *'type-5'* ) ]]; then
-	list_col='1'
-	list_row='6'
-elif [[ ( "$theme" == *'type-2'* ) || ( "$theme" == *'type-4'* ) ]]; then
-	list_col='6'
-	list_row='1'
-fi
-
-if [[ ( "$theme" == *'type-1'* ) || ( "$theme" == *'type-5'* ) ]]; then
-	efonts="Iosevka Nerd Font 11"
-else
-	efonts="Iosevka Nerd Font 28"
-fi
-
 # Options
-layout=`cat ${theme} | grep 'USE_ICON' | cut -d'=' -f2`
-if [[ "$layout" == 'NO' ]]; then
-    option_1="󰍺  Dual Display"
-	option_2="󰍹  Main Display"
-	option_3="󰍹  Secundary Display"
-else
-    option_1="󰍺"
-	option_2="󰍹"
-	option_3="󰍹"
-fi
+option_1="󰍹  Main Display"
+option_2="󰍺  Dual Display"
+option_3="󰍹  Secundary Display"
 
-# Rofi CMD
-rofi_cmd() {
-	rofi -theme-str "listview {columns: $list_col; lines: $list_row;}" \
-		-theme-str 'textbox-prompt-colon {str: "󰍹";}' \
-		-theme-str "element-text {font: \"$efonts\";}" \
-		-dmenu \
-		-p "$prompt" \
-		-mesg "$mesg" \
-		-markup-rows \
-		-theme ${theme}
+# Vicinae CMD
+vicinae_cmd() {
+	vicinae dmenu --placeholder "Select Display Option"
 }
 
 # Pass variables to rofi dmenu
-run_rofi() {
-	echo -e "$option_1\n$option_2\n$option_3" | rofi_cmd
+run_vicinae() {
+	echo -e "$option_1\n$option_2\n$option_3" | vicinae_cmd
 }
 
 # Execute Command
 run_cmd() {
 	if [[ "$1" == '--opt1' ]]; then
-        sh ~/.screenlayout/layouts/dual_display.sh
-	elif [[ "$1" == '--opt2' ]]; then
         sh ~/.screenlayout/layouts/main_display.sh
+	elif [[ "$1" == '--opt2' ]]; then
+        sh ~/.screenlayout/layouts/dual_display.sh
 	elif [[ "$1" == '--opt3' ]]; then
         sh ~/.screenlayout/layouts/secundary_display.sh
 	fi
 }
 
 # Actions
-chosen="$(run_rofi)"
+chosen="$(run_vicinae)"
 case ${chosen} in
     $option_1)
 		run_cmd --opt1
