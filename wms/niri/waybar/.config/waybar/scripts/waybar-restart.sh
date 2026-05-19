5703c87a4fcb64885e03ae98b7190ef11265787b
Erlan Rangel <erlanrangel@gmail.com>
2026-05-19 10:36:27 -0600

    wip

--- wms/niri/waybar/.config/waybar/scripts/waybar-restart.sh
+++ wms/niri/waybar/.config/waybar/scripts/waybar-restart.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-
-# Kill any running Waybar processes
-pkill -x waybar
-
-# Wait briefly to ensure Waybar is fully terminated
-sleep 1
-
-# Start Waybar again
-nohup waybar >/dev/null 2>&1 &
+../../../../../wayland-common/waybar/.config/waybar/scripts/waybar-restart.sh
